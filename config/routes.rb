9af449fa46eba3210ebc437cf42d3b187480a371
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-08-16 14:57:13 +0500

    Setup devise, devise api, and rack cores.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_for :users,
+                 controllers: { tokens: 'devise/api/tokens' },
+                 defaults: { format: :json }
+      # ... other API routes ...
+    end
+  end
 end
