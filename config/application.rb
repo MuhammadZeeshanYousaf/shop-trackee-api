4fcd0739245dafb0e863628b8b8ad1cde19a2dbc
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-08-26 15:27:16 +0500

    Setup test framework rspec-rails.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,11 @@ module TrackMyShopApi
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # customize rails generator workflow
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
