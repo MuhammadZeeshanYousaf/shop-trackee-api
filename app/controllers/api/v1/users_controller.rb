a6a33175fc548e732153d24f226102cc29e6989f
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-08-29 21:09:24 +0500

    Create Role base objects.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,9 +3,23 @@ class Api::V1::UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
+      role_based_obj = {}
+      if @user.customer?
+        @customer = Customer.find_or_initialize_by user: @user
+        @customer.assign_attributes customer_params
+        @customer.save
+        role_based_obj = { customer: @customer&.serializable_hash }
+      elsif @user.seller?
+        @seller = Seller.find_or_initialize_by user: @user
+        @seller.assign_attributes seller_params
+        @seller.save
+        role_based_obj = { seller: @seller&.serializable_hash }
+      end
+
       render json: {
         message: "#{@user.name}'s information updated successfully",
-        user: UserSerializer.new(@user).serializable_hash
+        user: UserSerializer.new(@user).serializable_hash,
+        **role_based_obj
       }
     else
       render json: {
@@ -20,6 +34,14 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.permit(:name, :gender, :country, :phone, :address, :avatar)
+  end
+
+  def customer_params
+    params.permit(:vocation, :age, :newsletter_subscribed)
+  end
+
+  def seller_params
+    params.permit(:intro, :rating)
   end
 
   def set_user
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,3 +1,7 @@
 class Seller < ApplicationRecord
   belongs_to :user
+
+  attribute :rating, :integer
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
+
 end
