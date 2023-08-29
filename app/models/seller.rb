48b8a4fec9786437320fe8388f8f7262a3fc47f1
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-08-29 22:19:51 +0500

    Create seller resources for crud.

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,5 +1,6 @@
 class Seller < ApplicationRecord
   belongs_to :user
+  has_many :shops
 
   attribute :rating, :integer
   validates :rating, numericality: { less_than_or_equal_to: 5 }
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,6 +4,7 @@ class Shop < ApplicationRecord
   array_enum closing_days: { 'Monday' => 0, 'Tuesday' => 1, 'Wednesday' => 2,
                              'Thursday' => 3, 'Friday' => 4, 'Saturday' => 5, 'Sunday' => 6 }
   validates :closing_days, subset: closing_days.keys
+  validates :name, uniqueness: { scope: :seller }
 
   belongs_to :seller
 
