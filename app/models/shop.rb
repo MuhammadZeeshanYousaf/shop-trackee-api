bfc2a32b63e7d944262fc5462f22b22cf419cf15
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-08-29 21:28:13 +0500

    Add validations on Shop and create array_enum.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,10 @@
 class Shop < ApplicationRecord
+  extend ArrayEnum
+
+  array_enum closing_days: { 'Monday' => 0, 'Tuesday' => 1, 'Wednesday' => 2,
+                             'Thursday' => 3, 'Friday' => 4, 'Saturday' => 5, 'Sunday' => 6 }
+  validates :closing_days, subset: closing_days.keys
+
+  belongs_to :seller
+
 end
--- db/migrate/20230829152436_create_shops.rb
+++ db/migrate/20230829152436_create_shops.rb
@@ -5,8 +5,8 @@ class CreateShops < ActiveRecord::Migration[7.0]
       t.text :description
       t.string :address, comment: 'Custom address of shop.'
       t.string :contact, comment: 'Business contact number.'
-      t.time :opening_time, comment: 'Format could be 16:57:19'
-      t.time :closing_time
+      t.time :opening_time, comment: 'Format could be 16:57:19 or 09:24PM'
+      t.time :closing_time, comment: 'Humanize the time like this: Time.now.strftime("%I:%M%p") => 09:24PM'
       t.integer :closing_days, array: true, null: false, default: [], comment: 'In these days the shop is closed, starting from Monday:0'
       t.string :social_links, array: true, default: []
       t.string :shop_website_url, comment: 'Online shop website url if exists.'
