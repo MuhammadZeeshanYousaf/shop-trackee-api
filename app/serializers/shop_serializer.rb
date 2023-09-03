d0d9809c92376bdc33f067093edae86318447439
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-09-03 23:03:20 +0500

    Respond with only time for Shop.

--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -1,4 +1,13 @@
 class ShopSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :address, :contact, :opening_time, :closing_time, :closing_days, :social_links, :shop_website_url
   has_one :seller
+
+  def opening_time
+    object.opening_time.strftime("%I:%M%p")
+  end
+
+  def closing_time
+    object.closing_time.strftime("%I:%M%p")
+  end
+
 end
