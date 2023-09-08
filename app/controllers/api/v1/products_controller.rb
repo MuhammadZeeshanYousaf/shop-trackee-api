4e714105a9dd4be990a02c829c7fca15f76fdab8
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-09-08 18:04:08 +0500

    Generalize the Product actions.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
+  include ShopItemsCommonActions
   before_action :set_shop, except: %i[ show update destroy ]
-  before_action :set_product, only: %i[ show update destroy create_or_upload replace_image remove_image recognize ]
+  before_action :set_product, only: %i[ show update destroy ]
 
   # GET /products
   def index
@@ -62,92 +63,6 @@ class Api::V1::ProductsController < ApplicationController
     else
       render json: { message: "Product with id: #{params[:id]} does not exist." }, status: :not_found
     end
-  end
-
-  # PUT /products/(:id)/images
-  def create_or_upload
-    if @product.present?
-      @product.images.attach(params[:images])
-    else
-      @product = @shop.products.new(images: params[:images])
-    end
-    @product.category ||= Category.where(category_type: Product.to_s).first
-
-    if @product.save(validate: false)
-      render json: {
-        message: 'Product image uploaded successfully',
-        product: ProductSerializer.new(@product).serializable_hash.slice(:id, :images)
-      }, status: :created, location: api_v1_shop_product_path(@shop, @product)
-    else
-      render json: {
-        message: 'Product image cannot be uploaded',
-        error: @product.errors.full_messages.to_sentence
-      }, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH /products/:id/images/:image_id/replace
-  def replace_image
-    if @product&.remove_image(params[:image_id])
-      @product.images.attach(params[:image])
-      if @product.save(validate: false)
-        return render(json: {
-          message: 'Image replaced successfully',
-          image: {
-            id: @product.images.last.id,
-            path: rails_blob_path(@product.images.last)
-          }
-        })
-      else
-        return render(json: {
-          message: 'Previous image removed but new image not updated',
-          error: @product.errors.full_messages.to_sentence
-        })
-      end
-    end
-
-    # if image not exist / not replaced
-    render json: {
-      message: 'Image not found',
-      error: @product&.errors&.full_messages&.to_sentence
-    }, status: :not_found
-  end
-
-  # DELETE /products/:id/images/:image_id
-  def remove_image
-    if @product&.remove_image(params[:image_id])
-      render json: { message: "Image deleted successfully" }
-    else
-      render json: { message: "Image not found with id: #{params[:image_id]} and product id: #{params[:id]}" }, status: :not_found
-    end
-  end
-
-  # GET /products/:id/images/:image_id/recognize
-  def recognize
-    if @product.present?
-      image_key = @product.get_image_key(params[:image_id])
-      image_data = AwsService::ImageRecognition.call(image_key)
-
-      if image_data.length > 0
-        @products = []
-        # Generate Product objects
-        image_data.each do |label_data|
-          category = Category.find_all_like(label_data[:categories]).first
-          category = Category.create(name: label_data[:categories].first, category_type: Product.to_s) if category.blank?
-          category = Category.where(category_type: Product.to_s).first if label_data[:categories].blank?
-          product = Product.new label_data.slice(:name, :description).merge(shop_id: @product.shop_id, category_id: category.id)
-
-          @products << product
-        end
-
-        return render(json: @products, each_serializer: ProductSerializer)
-      end
-    end
-
-    render json: {
-      message: 'Image not found',
-      error: @product&.errors&.full_messages&.to_sentence
-    }, status: :not_found
   end
 
 
