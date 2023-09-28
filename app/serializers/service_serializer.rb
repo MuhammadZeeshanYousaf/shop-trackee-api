d6df7b437b3ab99fbd458f184fae7e13394efe5d
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-09-28 18:35:27 +0500

    Include rate in services serializer.

--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -1,5 +1,5 @@
 class ServiceSerializer < ActiveModel::Serializer
   include ImagePathHelper
-  attributes :id, :name, :description, :published, :charge_by, :category_name, :images, :created_at, :updated_at
+  attributes :id, :name, :description, :published, :charge_by, :rate, :category_name, :images, :created_at, :updated_at
   has_one :shop
 end
