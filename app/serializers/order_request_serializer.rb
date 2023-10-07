667a135b68dc51f23cf79b094409abd74b8e989b
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-10-07 23:16:31 +0500

    Include user information with order request.

--- app/serializers/order_request_serializer.rb
+++ app/serializers/order_request_serializer.rb
@@ -1,12 +1,18 @@
 class OrderRequestSerializer < ActiveModel::Serializer
-  attributes :id, :message, :status, :created_at, :orderable_type
+  attributes :id, :message, :status, :created_at, :orderable_type, :customer
 
   has_one :orderable
   has_one :shop
-  has_one :customer
 
   def created_at
     object.created_at.strftime("%a, %d %b %Y%l:%M %p")
+  end
+
+  def customer
+    @customer = object.customer
+    @user = @customer.user
+
+    @user.serializable_hash(only: [:name, :email, :phone, :address, :gender]).merge(@customer.serializable_hash)
   end
 
 end
