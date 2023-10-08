5413a6c98af2e647438c9104ebbb877033a09d55
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-10-09 01:09:26 +0500

    Include is_favorite true to favoritable serializer relation.

--- app/serializers/favorite_serializer.rb
+++ app/serializers/favorite_serializer.rb
@@ -1,4 +1,9 @@
 class FavoriteSerializer < ActiveModel::Serializer
   attributes :id, :favoritable_type
   has_one :favoritable
+
+  def favoritable
+    ActiveModelSerializers::SerializableResource.new(object.favoritable).serializable_hash.merge(is_favorite: true)
+  end
+
 end
