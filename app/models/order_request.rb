08e9d27df94012bb797b9aa2599a2d0a9f68ff18
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-10-15 17:49:11 +0500

    Paginate Order Requests.

--- app/models/order_request.rb
+++ app/models/order_request.rb
@@ -2,8 +2,8 @@ class OrderRequest < ApplicationRecord
   belongs_to :customer
   belongs_to :shop
   belongs_to :orderable, polymorphic: true
-
   enum :status, { pending: 0, accepted: 1, rejected: 2 }
+  paginates_per 8
 
   default_scope { order(created_at: :desc) }
   # Scopes for removed_by attribute
