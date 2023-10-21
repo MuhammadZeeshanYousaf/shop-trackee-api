859fce52e9e5b9982a4c104ecb2c5ca148a79342
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-10-21 23:13:02 +0500

    Remove default role from user.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_21_104308) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_21_180840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,9 +55,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_21_104308) do
     t.string "vocation"
     t.integer "age"
     t.boolean "newsletter_subscribed", default: false
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_customers_on_user_id"
   end
 
@@ -130,9 +130,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_21_104308) do
   create_table "sellers", force: :cascade do |t|
     t.text "intro", comment: "A short introduction for customers."
     t.integer "rating", default: 0
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_sellers_on_user_id"
   end
 
@@ -184,7 +184,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_21_104308) do
     t.string "phone"
     t.text "address"
     t.string "avatar"
-    t.string "role", default: "customer"
+    t.string "role"
     t.string "password_reset_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["password_reset_token"], name: "index_users_on_password_reset_token", unique: true
