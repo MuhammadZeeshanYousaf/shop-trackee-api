462634f381455c85b9a1bd3dac3c1ad9e47abc83
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-10-22 16:25:55 +0500

    use query param key.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
     reset_token = @user.password_reset_token.present? ? @user.password_reset_token : @user.regenerate_password_reset_token
 
     @secure_account_url = secure_account_api_v1_user_url(reset_token)
-    @reset_password_url = "#{APP_HOST_URL}/reset-password/?#{reset_token}"
+    @reset_password_url = "#{APP_HOST_URL}/reset-password/?token=#{reset_token}"
 
     mail(to: email_address_with_name(@user.email, @user.name), subject: "Someone Request to reset password for #{@user.name} at #{APP_NAME}")
   end
