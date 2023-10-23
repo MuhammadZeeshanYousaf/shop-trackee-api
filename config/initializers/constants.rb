de9a7e3aec24d2861f36f87eccaa2a41ffaa0dc0
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-10-23 12:35:34 +0500

    Update logo url.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,4 @@
 DATETIME_FORMAT = "%a %b %d %Y %H:%M:%S GMT%z (%Z)"
 APP_NAME = 'Shop Trackee'
-APP_HOST_URL = 'https://www.shoptrackee.me'
+APP_HOST_URL = ENV['APP_HOST_URL']
+LOGO_URL = "#{APP_HOST_URL}/images/apple-touch-icon.png"
