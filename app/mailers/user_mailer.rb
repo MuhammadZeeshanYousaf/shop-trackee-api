24fd6191ad70904f13cf6acb118817230e1a5394
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-10-23 11:16:30 +0500

    Design Welcome email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,7 +12,7 @@ class UserMailer < ApplicationMailer
     @secure_account_url = secure_account_api_v1_user_url(reset_token)
     @reset_password_url = "#{APP_HOST_URL}/reset-password/?token=#{reset_token}"
 
-    mail(to: email_address_with_name(@user.email, @user.name), subject: "Someone Request to reset password for #{@user.name} at #{APP_NAME}")
+    mail(to: email_address_with_name(@user.email, @user.name), subject: "Request to reset password for #{@user.name}")
   end
 
 end
