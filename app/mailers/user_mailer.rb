412ce92165864689b8500624fe869c936c57626c
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-10-27 12:00:53 +0500

    Fix first reset token generation issue.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,11 @@ class UserMailer < ApplicationMailer
 
   def password_reset_email
     @user = params[:user]
-    reset_token = @user.password_reset_token.present? ? @user.password_reset_token : @user.regenerate_password_reset_token
+    reset_token = if @user.password_reset_token.present?
+                    @user.password_reset_token
+                  else
+                    @user.regenerate_password_reset_token && @user.password_reset_token
+                  end
 
     @secure_account_url = secure_account_api_v1_user_url(reset_token)
     @reset_password_url = "#{APP_HOST_URL}/reset-password/?token=#{reset_token}"
